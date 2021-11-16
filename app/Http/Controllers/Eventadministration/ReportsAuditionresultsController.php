04758aaf986bf17994c92a17087ceb6051a37c33
MFRHoldings <rick@mfrholdings.com>
2021-11-16 17:17:14 -0500

    testing pdf download in afdc-2021 production

--- app/Http/Controllers/Eventadministration/ReportsAuditionresultsController.php
+++ app/Http/Controllers/Eventadministration/ReportsAuditionresultsController.php
@@ -34,7 +34,8 @@ class ReportsAuditionresultsController extends Controller
             . '.auditionresults',
             compact('eventversion','registrants', 'scoringcomponents','score','scoresummary'))
             ->setPaper('letter', 'portrait');;
-
+$pdf = PDF::loadView('testpdf');
+return $pdf->download('testpdf.pdf');
         //log application printing
         //Application::create([
         //    'registrant_id' => $registrant->id,

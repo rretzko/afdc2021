b5784394209a7a63975192a50f7f4be599833545
MFRHoldings <rick@mfrholdings.com>
2021-11-23 17:57:31 -0500

    Hardcode prod link to download timeslots-component.blade.php

--- resources/views/livewire/timeslots-component.blade.php
+++ resources/views/livewire/timeslots-component.blade.php
@@ -1,7 +1,13 @@
 <div style="margin-bottom: -1rem;">
-    <a href="{{ route('registrationmanagers.timeslotassignment.download',['eventversion' => $eventversion]) }}">
-        Download Assigned Timeslots
-    </a>
+    @if(config('app.url') === 'http://afdc2021.test')
+        <a href="{{ route('registrationmanagers.timeslotassignment.download',['eventversion' => $eventversion]) }}">
+            Download Assigned Timeslots
+        </a>
+    @else
+        <a href="https://afdc-2021-l38q8.ondigitalocean.app/registrationmanager/timeslots/download/{{ $eventversion->id }}">
+            Download Assigned Timeslots
+        </a>
+    @endif
 
 </div>
 <div>

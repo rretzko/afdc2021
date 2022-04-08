47cc860d5e634aa5f49d2ac106afa9249b2b15e6
MFRHoldings <rick@mfrholdings.com>
2022-04-08 13:49:42 -0400

    Removing commented-out verbiage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -143,26 +143,7 @@
                 If you build your auditioned events using spreadsheets, docs, or any other system
                 which is paper-and-time-intensive, we invite you to take a look!
             </div>
-<!-- {{--
-            <div class="demo ">
-                <button href="{{ route('demo') }}" title="Click for our demo pages">
-                    Click here for our no-obligation demo!
-                </button>
-            </div>
-        </div>
---}} -->
-        <!--
-                        <div class="links">
-                            <a href="https://laravel.com/docs">Docs</a>
-                            <a href="https://laracasts.com">Laracasts</a>
-                            <a href="https://laravel-news.com">News</a>
-                            <a href="https://blog.laravel.com">Blog</a>
-                            <a href="https://nova.laravel.com">Nova</a>
-                            <a href="https://forge.laravel.com">Forge</a>
-                            <a href="https://vapor.laravel.com">Vapor</a>
-                            <a href="https://github.com/laravel/laravel">GitHub</a>
-                        </div>
-        -->
+
     </div>
 </div>
 </body>

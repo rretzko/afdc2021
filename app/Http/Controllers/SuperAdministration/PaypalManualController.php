df13b1604003bbf0ac5db78d1333c9ea8f6e6c35
Retzko <rick@mfrholdings.com>
2022-10-27 08:37:11 -0400

    Update PaypalManualController.php

--- app/Http/Controllers/SuperAdministration/PaypalManualController.php
+++ app/Http/Controllers/SuperAdministration/PaypalManualController.php
@@ -3,40 +3,16 @@
 namespace App\Http\Controllers\SuperAdministration;
 
 use App\Http\Controllers\Controller;
-use App\Models\Teacher;
 use Illuminate\Http\Request;
 
-class LogInAsController extends Controller
+class PaypalManualController extends Controller
 {
-    /**
-     * Show form for select user to log in as
-     */
-    public function edit()
-    {
-        $collection = Teacher::with('person')->get()->sortBy('person.last');
-
-        $teachers = [];
-        foreach($collection AS $teacher){
-            $teachers[] = [$teacher['person']->user_id,$teacher['person']->fullNameAlpha()];
-        }
-
-        //sa = SuperAdministrator
-        return view('sa.loginas.edit', compact('teachers'));
-    }
-
+    
     /**
      * Update state based on user_id
      */
     public function update(Request $request)
     {
-        $input = $request->validate(
-            [
-                'user_id' => ['numeric','required','exists:people']
-            ]
-        );
-
-        auth()->loginUsingId($input['user_id']);
-
-        return redirect()->route('home');
+        return view('sa.paypals.update',['registrant' => 'No registrant found.']);
     }
 }
